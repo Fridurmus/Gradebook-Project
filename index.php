18f3cf3bca9364dad7838af1c2859ff2937713e4
Sean Davis <Fridurmus@gmail.com>
2017-01-13 10:11:58 -0500

    Add SQL & build HTML Table
    Added SQL query and PHP to dynamically build an HTML table from the
    SELECT.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -7,4 +8,41 @@
  */
 require_once 'includes/database_functions.php';
 
-echo "Hello!";
+$sql = "INSERT INTO gradebook (assign_name, grade_earned, grade_max)
+        VALUES (:assign_name, :grade_earned, :grade_max)";
+
+$vars = array(':assign_name'=>'test_assign2', ':grade_earned'=>80.5, ':grade_max'=>100.0);
+
+
+
+//$res = pdoUpdate($sql, $vars);
+
+//echo "<pre>";
+//print_r($res);
+$gradebookRows = pdoSelect('SELECT * FROM gradebook');
+?>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Assignment Name</th>
+                <th>Grade Earned</th>
+                <th>Possible Grade</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+    foreach($gradebookRows as $gradebookRow){
+    extract($gradebookRow);
+    echo "<tr>
+      <td>$assign_id</td>
+      <td>$assign_name</td>
+      <td>$grade_earned</td>
+      <td>$grade_max</td>
+      </tr>
+    ";
+}
+?>
+        </tbody>
+    </table>
+</body>
